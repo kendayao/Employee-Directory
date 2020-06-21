3af496e8550a72741efca678b105a0f855b1b528
Kaeneth Dayao <kendayao@gmail.com>
2020-06-21 11:36:14 -0700

    form.js setup and Rednerlist.js add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react"
-
+import Form from "./components/Form"
 function App(){
  return (<div>
      <h1>Employee Directory</h1>
+    <Form />
  </div>)
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Form(){
+return <form>
+    <input type="text" 
+    placeholder="Search">
+    </input>
+</form>
+
+}
+
+export default Form;
