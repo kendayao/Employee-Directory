96dbba5344606a45b87122741673294f897bde92
Kaeneth Dayao <kendayao@gmail.com>
2020-06-21 12:29:06 -0700

    added the form states

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Form(){
+
+const [SearchName, SetSearchName]=useState("")
+
+function handleInput(event){
+    const value =event.target.value
+    SetSearchName(value)
+}
+
+
+
+
+
 return <form>
-    <input type="text" 
+    <h1>{SearchName}</h1>
+    <input type="text"
+    name="searchName"
+    onChange={handleInput}
+    value={SearchName}
     placeholder="Search">
     </input>
+    <button>Submit</button>
 </form>
 
 }
