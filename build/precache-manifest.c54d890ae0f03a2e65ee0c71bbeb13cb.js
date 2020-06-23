fe266e69223adfad92df2e25c0eb28ebd74851c8
Kaeneth Dayao <kendayao@gmail.com>
2020-06-23 16:28:37 -0700

    push to pages

---
+++ build/precache-manifest.c54d890ae0f03a2e65ee0c71bbeb13cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462b6f4fa19bfbb560e43a3350eeef87",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "5d8a6f322934d2ef5b7b",
+    "url": "/Employee-Directory/static/css/main.b966dbf2.chunk.css"
+  },
+  {
+    "revision": "92f7bdec2c233bf7ecf3",
+    "url": "/Employee-Directory/static/js/2.80a7bcf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.80a7bcf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8a6f322934d2ef5b7b",
+    "url": "/Employee-Directory/static/js/main.cbcd4041.chunk.js"
+  },
+  {
+    "revision": "52e1e8e23e7dfaf64514",
+    "url": "/Employee-Directory/static/js/runtime-main.6ca9f082.js"
+  }
+]);
